f41ba346cecf6b495a8c0637d6bcc2d875e2f54f
Alexander Holmberg <Holmb.alexander@gmail.com>
2016-10-12 13:54:53 +0200

    intital commit

---
+++ Projects/Dog/Dog/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+        public Dog()
+        {
+        }
+
+        public Dog(string name, int age, string breed)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+        }
+
+        public string DogIntroduction()
+        {
+
+            return String.Format("This {0}´s name is {1} and is {2} years old.", Breed, Name, Age);
+        }
+    }
+}
---
+++ Projects/Dog/Dog/Runtime.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    class Runtime
+    {
+        List<Dog> dogs = new List<Dog>()
+            {
+            new Dog { Name = "Fido", Age = 6, Breed = "Pitbull" },
+            new Dog { Name = "Bubben", Age = 2, Breed = "Golden Retriver" },
+            new Dog { Name = "Schmitzel", Age = 4, Breed = "Weiner" },
+            };
+
+        public void Start()
+        {
+            Menu();
+
+        }
+        private void Menu()
+        {
+            bool loop = false;
+            do
+            {
+                loop = false;
+                Console.WriteLine("Press a number beetwen 1-4 to continue...");
+                var input = Console.ReadKey(true).Key;
+
+                switch (input)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Add dog");
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Remove dog");
+                        break;
+                    case ConsoleKey.D3:
+                        ShowDogs();
+                        break;
+                    case ConsoleKey.D4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        loop = true;
+                        break;
+                }
+            } while (loop);
+        }
+        void ShowDogs()
+        {
+            foreach (var Dog in dogs)
+            {
+                Console.WriteLine(Dog.DogIntroduction());
+                Console.WriteLine();
+            }
+        }
+    }
+}
