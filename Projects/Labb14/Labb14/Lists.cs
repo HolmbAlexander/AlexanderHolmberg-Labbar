23bbf0f9498244a3e0c799024f3b4309437dffc0
Alexander Holmberg <Holmb.alexander@gmail.com>
2016-11-18 10:01:08 +0100

    Added error managment

--- Projects/Labb14/Labb14/Lists.cs
+++ Projects/Labb14/Labb14/Lists.cs
@@ -11,7 +11,7 @@ namespace Labb14
         public List<Author> authorsList { get; set; }
         public List<Book> booksList { get; set; }
         public List<Magazine> magazinesList { get; set; }
-        public List<Paper> Papers { get; set; }
+        public List<Paper> paperList { get; set; }
 
         public Lists()
         {
@@ -27,13 +27,20 @@ namespace Labb14
 
             booksList = new List<Book>
             {
-                new Book { Author = authorsList[0], Title = "Harry Potter and the Sorcerer's Stone", Pages = 231, RealseDate = new DateTime(1997) },
+                new Book { Author = authorsList[0], Title = "Harry Potter and the Sorcerer's Stone", Pages = 231, RealseDate = new DateTime(1997,03,09) },
                 new Book { Author = authorsList[1], Title = "Fight Club", Pages = 258, RealseDate = new DateTime(1996,08,17) },
             };
 
             magazinesList = new List<Magazine>
             {
-                new Magazine { Author = authorsList[2], Title = "American Pshyco", RealseDate = new DateTime() }
+                new Magazine { Author = authorsList[2], Title = "American Pshyco", RealseDate = new DateTime(1991,01,10) },
+                new Magazine { Author = authorsList[3], Title = "The Handmaid's Tale", RealseDate = new DateTime(1985,05,25) }
+            };
+
+            paperList = new List<Paper>
+            {
+                new Paper { Author = authorsList[4], Title = "Revolutionary Road", RealseDate = new DateTime(1961,11,29) },
+                new Paper { Author = authorsList[5], Title = "Fear of Flying", RealseDate = new DateTime(1973,08,21) }
             };
         }
     }
--- Projects/Labb14/Labb14/Runtime.cs
+++ Projects/Labb14/Labb14/Runtime.cs
@@ -8,12 +8,118 @@ namespace Labb14
 {
     class Runtime
     {
+        Book book = new Book();
+        Magazine magazine = new Magazine();
+        Paper paper = new Paper();
+        Author author = new Author();
+        Lists listManager = new Lists();
+
         public void Start()
         {
-            Book book = new Book();
-            Magazine magazine = new Magazine();
-            Paper paper = new Paper();
-            Author author = new Author();
+            bool loop = true;
+            while (loop)
+            {
+                Console.Clear();
+                Console.WriteLine("1: Search for author");
+                Console.WriteLine("2: See list of authors and there work");
+                Console.WriteLine("3: Exit");
+                var input = Console.ReadKey(true).Key;
+                switch (input)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        ShowAuthors();
+                        SearchAuthor();
+                        Console.ReadKey(true);
+                        break;
+
+                    case ConsoleKey.D2:
+                        ShowAuthorAndWork();
+                        break;
+
+                    case ConsoleKey.D3:
+                        loop = false;
+                        break;
+                }
+            }
+        }
+
+        void ShowAuthors()
+        {
+            int i = 1;
+            foreach (var author in listManager.authorsList)
+            {
+                Console.WriteLine(i + ": " + author.Name); i++;
+            }
+        }
+
+        void ShowAuthorAndWork()
+        {
+            Console.Clear();
+            int i = 1;
+            foreach (var author in listManager.booksList)
+            {
+                Console.WriteLine(i + ": " + "Author: {0}. Title: {1}. Pages: {2}. Realse date: {3}. ",
+                                 author.Author.Name,
+                                 author.Title,
+                                 author.Pages,
+                                 author.RealseDate.ToShortDateString()); i++;
+            }
+            foreach (var author in listManager.magazinesList)
+            {
+                Console.WriteLine(i + ": " + "Author: {0}. Title: {1}. Realse date: {2}. ",
+                                 author.Author.Name,
+                                 author.Title,
+                                 author.RealseDate.ToShortDateString()); i++;
+            }
+            foreach (var author in listManager.paperList)
+            {
+                Console.WriteLine(i + ": " + "Author: {0}. Title: {1}. Realse date: {2}. ",
+                                 author.Author.Name,
+                                 author.Title,
+                                 author.RealseDate.ToShortDateString()); i++;
+            }
+            Console.ReadKey(true);
+        }
+
+        void SearchAuthor()
+        {
+            Console.Write("Enter name or part of name from author to see their work: ");
+            string nameInput = Console.ReadLine();
+
+            var getBooks = listManager.booksList.Where(author => author.Author.Name.Contains(nameInput));
+            var getMagazines = listManager.magazinesList.Where(author => author.Author.Name.Contains(nameInput));
+            var getPapers = listManager.paperList.Where(author => author.Author.Name.Contains(nameInput));
+
+            if (getBooks.Any() || getMagazines.Any() || getPapers.Any())
+            {
+                foreach (var author in getBooks)
+                {
+                    Console.WriteLine("Author: {0}. Title: {1}. Pages: {2}. Realse date: {3}. ",
+                                     author.Author.Name,
+                                     author.Title,
+                                     author.Pages,
+                                     author.RealseDate.ToShortDateString());
+                }
+
+                foreach (var author in getMagazines)
+                {
+                    Console.WriteLine("Author: {0}. Title: {1}. Realse date: {2}. ",
+                                     author.Author.Name,
+                                     author.Title,
+                                     author.RealseDate.ToShortDateString());
+                }
+
+                foreach (var author in getPapers)
+                {
+                    Console.WriteLine("Author: {0}. Title: {1}. Realse date: {2}. ",
+                                     author.Author.Name,
+                                     author.Title,
+                                     author.RealseDate.ToShortDateString());
+                }
+            }
+            else
+                Console.WriteLine("No author with that name found.");
         }
     }
 }
